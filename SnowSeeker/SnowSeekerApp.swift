ce25c27308c5d9f9c2ce6496c1d8ad71a231cdcb
Artem Bolotov <teeemon@gmail.com>
2023-07-19 08:13:32 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by artembolotov on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

0d4c7521e640eb0dc57be9dc0f2c9ff2fb162853
Artem Bolotov <teeemon@gmail.com>
2023-08-01 08:23:36 +0300

    Implemented

---
+++ SnowSeeker/Favorites.swift
@@ -0,0 +1,39 @@
+//
+//  Favorites.swift
+//  SnowSeeker
+//
+//  Created by artembolotov on 31.07.2023.
+//
+
+import Foundation
+
+class Favorites: ObservableObject {
+    private var resorts: Set<String>
+    private let saveKey = "Favorites"
+    
+    init() {
+        // load saved data
+        
+        resorts = []
+    }
+    
+    func containts(_ resort: Resort) -> Bool {
+        resorts.contains(resort.id)
+    }
+    
+    func add(_ resort: Resort) {
+        objectWillChange.send()
+        resorts.insert(resort.id)
+        save()
+    }
+    
+    func remove(_ resort: Resort) {
+        objectWillChange.send()
+        resorts.remove(resort.id)
+        save()
+    }
+    
+    func save() {
+        // write out our data
+    }
+}
---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by artembolotov on 24.07.2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+            
+            Text("Please select a resort from left-hand menu. Swipe from the left edge to show it.")
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
